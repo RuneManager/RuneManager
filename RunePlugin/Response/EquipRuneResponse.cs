34297ca7e1239251c9cb951c547f06c1eea34664
Skibisky <skibisky@outlook.com.au>
2017-07-09 09:42:25 +0800

    Shuffled plugins into a folder, VS doesn't do it for solutions? Prep for sending Rune changes to Manager.

---
+++ RunePlugin/Response/EquipRuneResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RunePlugin.Response
+{
+	public class EquipRuneResponse : SWResponse
+	{
+		[JsonProperty("rune_id")]
+		public ulong RuneId;
+
+		[JsonProperty("unit_info")]
+		public RuneOptim.Monster Monster;
+
+
+	}
+}
---
+++ plugins/RuneManagerBridge/RuneManagerBridge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RunePlugin;
+
+namespace RuneManagerBridge
+{
+	public class RuneManagerBridge : SWPlugin
+	{
+		public override void ProcessRequest(object sender, SWEventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
