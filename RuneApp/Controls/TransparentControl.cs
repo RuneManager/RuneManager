2350b862901dce84f67ef1152892d466abe8190b
Skibisky <skibisky@outlook.com.au>
2017-10-26 22:36:25 +0800

    Added Buff icons.

--- RuneApp/Controls/TransparentControl.cs
+++ RuneApp/Controls/TransparentControl.cs
@@ -2,95 +2,93 @@
 using System.Windows.Forms;
 using System.Drawing;
 
-namespace RuneApp
-{
-    // Maybe from http://stackoverflow.com/questions/395256/transparent-images-with-c-sharp-winforms
-    public sealed class TransparentControl : Control
-    {
-        private readonly Timer refresher;
-        private Image _image;
-        private float gamma;
+namespace RuneApp {
+	// Maybe from http://stackoverflow.com/questions/395256/transparent-images-with-c-sharp-winforms
+	public sealed class TransparentControl : Control {
+		private readonly Timer refresher;
+		private Image _image;
+		private float gamma;
+		private float scale;
 
-        public TransparentControl()
-        {
-            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-            BackColor = Color.Transparent;
-            gamma = 1;
-            refresher = new Timer();
-            refresher.Tick += TimerOnTick;
-            refresher.Interval = 50;
-            refresher.Enabled = true;
-            refresher.Start();
-        }
+		public TransparentControl() {
+			SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+			BackColor = Color.Transparent;
+			gamma = 1;
+			scale = 1;
+			refresher = new Timer();
+			refresher.Tick += TimerOnTick;
+			refresher.Interval = 50;
+			refresher.Enabled = true;
+			refresher.Start();
+		}
 
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams cp = base.CreateParams;
-                cp.ExStyle |= 0x20;
-                return cp;
-            }
-        }
+		protected override CreateParams CreateParams {
+			get {
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x20;
+				return cp;
+			}
+		}
 
-        protected override void OnMove(EventArgs e)
-        {
-            RecreateHandle();
-        }
+		protected override void OnMove(EventArgs e) {
+			RecreateHandle();
+		}
 
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            if (_image == null) return;
+		protected override void OnPaint(PaintEventArgs e) {
+			if (_image == null) return;
 
-            var attr = new System.Drawing.Imaging.ImageAttributes();
+			var attr = new System.Drawing.Imaging.ImageAttributes();
 
-            attr.SetGamma(gamma);
+			attr.SetGamma(gamma);
 
-            // slight change to how they drew it
-            e.Graphics.DrawImage(_image, new Rectangle((Width / 2) - (_image.Width / 2), (Height / 2) - (_image.Height / 2), _image.Width, _image.Height), 0, 0, _image.Width, _image.Height, GraphicsUnit.Pixel, attr);
-        }
+			// slight change to how they drew it
+			int width = (int)(_image.Width * scale);
+			int height = (int)(_image.Height * scale);
 
-        protected override void OnPaintBackground(PaintEventArgs e)
-        {
-            //Do not paint background
-        }
+			e.Graphics.DrawImage(_image, new Rectangle((Width / 2) - (width / 2), (Height / 2) - (height / 2), width, height), 0, 0, _image.Width, _image.Height, GraphicsUnit.Pixel, attr);
+		}
 
-        //Hack
-        public void Redraw()
-        {
-            RecreateHandle();
-        }
+		protected override void OnPaintBackground(PaintEventArgs e) {
+			//Do not paint background
+		}
 
-        private void TimerOnTick(object source, EventArgs e)
-        {
-            RecreateHandle();
-            refresher.Stop();
-        }
+		//Hack
+		public void Redraw() {
+			RecreateHandle();
+		}
 
-        public Image Image
-        {
-            get
-            {
-                return _image;
-            }
-            set
-            {
-                _image = value;
-                RecreateHandle();
-            }
-        }
+		private void TimerOnTick(object source, EventArgs e) {
+			RecreateHandle();
+			refresher.Stop();
+		}
 
-        public float Gamma
-        {
-            get
-            {
-                return gamma;
-            }
-            set
-            {
-                gamma = value;
-            }
-        }
-    }
+		public Image Image {
+			get {
+				return _image;
+			}
+			set {
+				_image = value;
+				RecreateHandle();
+			}
+		}
+
+		public float Gamma {
+			get {
+				return gamma;
+			}
+			set {
+				gamma = value;
+			}
+		}
+
+		public float Scale {
+			get {
+				return scale;
+			}
+			set {
+				scale = value;
+			}
+		}
+	}
 }
